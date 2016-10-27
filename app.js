00253c792058ae79e2b22d56b835d5e71e9f7b9a
vitaliy ivanchyshyn <vivanchyshyn@softjourn.com>
2016-10-27 13:08:06 +0300

    listen

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-/*var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});*/
-
-
-
 var app = require('express')();  
 var server = require('http').Server(app);  
 var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {  
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {  
+    socket.emit('announcements', { message: 'A new user has joined!' });
 });
 
 server.listen(8080);  
