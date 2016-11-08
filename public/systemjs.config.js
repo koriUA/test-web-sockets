51368c2628556af54ae21cc629f7c2b66fb27324
Vitaliy Ivanchyshyn <vivanchyshyn@softjourn.com>
2016-11-08 15:34:47 +0200

    ng2

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -29,12 +29,14 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: 'app/main.js',
+        main: 'main.js',
         defaultExtension: 'js'
       },
       rxjs: {
         defaultExtension: 'js'
       }
-    }
+    },
+    transpiler: 'typescript',
+    typescriptOptions: { emitDecoratorMetadata: true }
   });
 })(this);
