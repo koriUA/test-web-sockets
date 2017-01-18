60b1f4af4902e0e11542b2e1564c573c6c5dd5e1
Vitaliy Ivanchyshyn <vivanchyshyn@softjourn.com>
2017-01-18 19:00:43 +0200

    changes

--- app.js
+++ app.js
@@ -1,63 +1,92 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
 
+/**
+ * Module dependencies.
+ */
 
-var app = express();
-var expressWs = require('express-ws')(app);
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var app = require('./application');
+var debug = require('debug')('westernapp:server');
+var http = require('http');
 
+/**
+ * Get port from environment and store in Express.
+ */
 
-// view engine setup
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+console.log('listen ', port);
+app.listen(3001);		//for websocket...;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));  //share node_modules with front-end;
+/**
+ * Create HTTP server.
+ */
 
-app.use('/', routes);
-app.use('/users', users);
+var server = http.createServer(app);
 
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-// error handlers
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
-module.exports = app;
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
