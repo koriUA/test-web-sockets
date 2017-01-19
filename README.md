817915dd034cfb4e69750d3338bb9c944424baf2
vitaliy <kori.if.ua@gmail.com>
2017-01-19 21:33:18 +0200

    changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 npm install
 
-node app.js
+npm start
--- app.js
+++ app.js
@@ -1,35 +1,123 @@
 #!/usr/bin/env node
 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug')('westernapp:server');
+var http = require('http');
+var port = normalizePort(process.env.PORT || '3000');
+
+
+
+var app = express();
+app.set('port', port);
+console.log('listen ', port);
+//var expressWs = require('express-ws')(app);
+
+
+
+var server = http.createServer(app);
+var expressWs = require('express-ws')(app, server);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));  //share node_modules with front-end;
+
+app.use('/', routes);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+//module.exports = app;
+
+
+
+
+
+
+
+
+
+
 /**
  * Module dependencies.
  */
 
-var app = require('./application');
-var debug = require('debug')('westernapp:server');
-var http = require('http');
+//var app = require('./application');
+
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-console.log('listen ', port);
-app.listen(3001);		//for websocket...;
+
+//app.set('port', port);
+//console.log('listen ', port);
+//app.listen(3001);		//for websocket...;
+
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
-
+//var server = http.createServer(app);
+//var expressWs = require('express-ws')(app, server);
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+//server.listen(port);
+//server.on('error', onError);
+//server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
