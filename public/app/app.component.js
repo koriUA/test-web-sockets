ace26c831e4b4b6bba6da2a5ca11d6d294bd5de3
vitaliy <kori.if.ua@gmail.com>
2017-01-23 22:27:42 +0200

    added websocket service

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -1,4 +1,7 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import Rx from 'rxjs/Rx';
+import {Component, ChangeDetectorRef} from '@angular/core';
+import {WebSocketService} from 'app/services/web-socket.service.js'
+
 @Component({
   selector: 'my-app',
   templateUrl: '/app/app.html',
@@ -6,30 +9,25 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 })
 export class AppComponent {
 
+	private ws: Rx.Subject<MessageEvent>;
 	public messages = [];
 
-	constructor(public changeDetectorRef: ChangeDetectorRef){
-		//this.ws = new WebSocket('ws://localhost:3000/echo/');
-		this.ws = new WebSocket('ws://node.western.pp.ua/echo/');
-		var that = this;
-		this.ws.onmessage = function(event) {
-			console.log("message", event.data);
-			that.messages.push(event.data);
-			that.changeDetectorRef.detectChanges();	//fixed bug for Edge; TODO investigate for removing it;
-		};
-		this.ws.onopen = function(event){
-			console.log("websocket connection open");
-		};
-		this.ws.onerror = function(error){
-			console.log('WebSocket Error ', error);
-		};
+	constructor(
+		public webSocketService: WebSocketService,
+		public changeDetectorRef: ChangeDetectorRef
+		){
+		this.ws = this.webSocketService.connect('ws://node.western.pp.ua/echo/');
+		this.ws.subscribe( data => {
+			this.messages.push(event.data);
+			this.changeDetectorRef.detectChanges();	//fixed bug for Edge; TODO investigate for removing it;
+		})
 	}
 
 	send(input: HTMLInputElement): void {
 		if (!input.value){
 			return;
 		}
-		this.ws.send(input.value);
+		this.ws.next(input.value);
 		input.value = '';
 	}
 
--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,11 +1,17 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule}    from '@angular/forms';
-import { AppComponent }   from './app.component';
+import {AppComponent}   from './app.component';
+import {WebSocketService} from 'app/services/web-socket.service.js'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	imports: [BrowserModule, FormsModule],
+	declarations: [
+		AppComponent
+	],
+	providers: [
+		WebSocketService
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
